8c87db446643062e4da90ba421730a5704f1d45f
nayounghye <yhna.dev@gmail.com>
2024-04-01 23:59:32 +0900

    ✨ 카테고리 구성

--- PORTFOLIO/src/components/Main/MainAni.js
+++ PORTFOLIO/src/components/Main/MainAni.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const MainAni = () => {
-  return <div>MainAni</div>;
+  return (
+    <div>
+      <h1>NAPORTFOLIO</h1>
+      <h1>INFO</h1>
+      <h1>PROJECT</h1>
+    </div>
+  );
 };
 
 export default MainAni;
