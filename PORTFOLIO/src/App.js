de2b5a7e95c729553b4378421b8b9f5d9d1074d4
nayounghye <yhna.dev@gmail.com>
2024-07-06 22:29:28 +0900

    ✨ 메인화면 구성

--- PORTFOLIO/src/App.js
+++ PORTFOLIO/src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <>
       <MainPage />
-      <InfoPage />
-      <ProjectPage />
+      {/* <InfoPage /> */}
+      {/* <ProjectPage /> */}
     </>
   );
 }
--- PORTFOLIO/src/pages/Main/MainPage.js
+++ PORTFOLIO/src/pages/Main/MainPage.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import '../../styles/style.scss';
 
 const MainPage = () => {
   return (
     <>
-      <h1>MainPage</h1>
+      <div class="mainContainer">
+        <img class="img-spaceship" src="/static/spaceship.png" alt="image" />
+        <div class="stars">
+          <div class="stars1"></div>
+          <div class="stars2"></div>
+          <div class="stars3"></div>
+        </div>
+      </div>
     </>
   );
 };
